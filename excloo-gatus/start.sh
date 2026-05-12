7b27cb36125240e043ad8b1d7694a4941b0cf978
Max Schaefer <maxexcloo@users.noreply.github.com>
2026-05-12 21:40:24 +1000

    Update gatus-fly start.sh

--- excloo-gatus/start.sh
+++ excloo-gatus/start.sh
@@ -1,14 +1,14 @@
 {
-	"data": "ENC[AES256_GCM,data:VBIUnD8NedxsukhAexBTh+ABN6fLUnd6qIxf273oQbamKCc0E25gaWqx0eJq38BbMHqMZbwQ1PRUpitsdPT9997hMQhytUrS6h8k1xKwNGssE/Wc1/lQzUWWqWN3O+dlCNw/XzuP+xjL22yB0ceF01HsQQ51ckwGJf5/sAGpz7W9lqs00LiuZnDZLqj9xmy81QRCoec+0b0mGcCibR+jn9GWil/yrISKVoBpEC/5GGfA6Zi+mbHmdC57EmrGW7Vkrb1Wlsc8Z5ygM9bomkkzFubYgwaP5hZ3mJFQT1hCdsnWBHuzcm5S95R2pIdTjdw744ie,iv:1aO5HMHDS7ydD0VrdHCjs6XBSXpLxGXZRjknqVUFO08=,tag:c74I2Q4pACZ0MJQBssYOtw==,type:str]",
+	"data": "ENC[AES256_GCM,data:UgS43UBPgk62+wkzVvYqiZvYHX0mZnFCwF37fVqgsypDzFjYjkzy4fFPVKKXDfvllmBTy0aw32bza9GsnCB3gkqv8NL2oMBq88vtu/4lJbHC3w73Enf6q0i3T1OjY9lVa+c+lSTYKGq/J2JJ4y4wJdl2nJRwxOPFgVamvbp+wt5finfavBSaG8EZQ0bIIOO8vZ5zHxjBbLNmafXpoiUl/7ri4cExp6hDsPq19NKRqtMC/JqWzSbMh+GrxSQWJVfBbed4q8IrUuwJVcl6OipIQkVq0pcA8GvxEp42Q0bFCrvlBYPtBRGeEvVLA+wwfPZIP6cQ,iv:LTEqZp3ACEi1iU4zpgdLl4j5jQgiJ7jEKEHbuuyqjaU=,tag:jMbTjwCjcw7piRdFF1Xsaw==,type:str]",
 	"sops": {
 		"age": [
 			{
 				"recipient": "age1cnktwwj4lq4lm5qa3vxn2ft5n38h557smr2f58t9vz4zum5w0udqzzle6c",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSA2Y2U3UVBSV0txcWhEMWRv\nYUJySjV2c0RjeUpTNzE3ZkFlZ2tDQyt0ZHc0CkJMemRXTFd3a3ZHR284VExRVWVO\nMGVSUGdqUS81VGYzR2RtY0x6ME5ZM2cKLS0tIE1oWmFjZjlzK3R3dmNvQzI4Mkdl\nNkdCT1N6Q0Jrd0hSZXZQT0Myc0ZhWnMKG6ixSA9Rzf1JLPddCcP4xNQx6SboJo9k\nYwp5370J9n8E0QWyhwfptgfVt8H3NAgAWrZ7l8uqGhvzdnWnD/bCdw==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSB5V2hxU0QvYzYvckQ0dUlp\nRUZta0xMNGNWRnBLbXUxbHhUZC9BWHUreTFjCkJhK0hyZDVRZDJUcXdtWm8xR211\nTjhYcFo5SElrZ0VyaVovTCtLcWt3TnMKLS0tIDJOMk03VStkdDliZEFmbzMvREsv\nMG8yTjk0SnA0VVdDQXcxRkI0UmdCa0EKhwzTBI9iThjs1/2nNKVOGg+KumUXwLCj\n+goErzd+aUBzXUMJFuWqCwHSWbjqw7gwxvgag/8A4DiNedW+Jpptcw==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2026-05-12T03:03:46Z",
-		"mac": "ENC[AES256_GCM,data:p4U6V9k3JoFPLkz+GjK+4AWz9BTuRcVijYkC907uyO2K28MNPQ2r1gZdGGqk3/enG1AXSntPikOcKW3s2SO0uXWPOmxL7ix5OMQKhBl810vsP4o5wN6JD0CqI6D+0C7Bkq4OUsSNPFkd/KfvyEfZmzqKTDh0ZpFOT5G/W/aOMs8=,iv:nMh8SSf6AFELlivhgMZ0kIpN/Awdsv2lNId0Z/rczQQ=,tag:KmyQEiqpiCEg006Yarl9DQ==,type:str]",
+		"lastmodified": "2026-05-12T11:40:09Z",
+		"mac": "ENC[AES256_GCM,data:2AqebyobN/2e8rKQhtHc08H8VJFS3CcH2BxUCz1q5+nDiZTHRzr9gspq1jBeJ00zMD/6mj2mBSWZpsyeUv/DUX3YhY4/dq0XdvxCTa2+jpspcPK7kn9WH6FOKVIdG8cZ/ZY5pLxAx2ekZpxDSij108IGa43MO2fylWOlequI6w0=,iv:cFNWnW3ISya6aMf6A/WjTWFuI9UVBLZ4r65Y5rgazto=,tag:DYPTfeyOuZfJ9uzXlgs40g==,type:str]",
 		"version": "3.12.2"
 	}
 }
